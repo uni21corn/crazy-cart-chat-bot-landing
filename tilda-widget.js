a5138988116763e47d0ea39f37f2497055af0627
uni21corn <d.grebenkova2@gmail.com>
2026-05-14 14:30:00 +0300

    Update success CTA link and message

--- tilda-widget.js
+++ tilda-widget.js
@@ -6,7 +6,8 @@
   const widgetRootId = "crazy-cart-chat-landing";
   const tildaFormSelector = window.CRAZY_CART_TILDA_FORM_SELECTOR || ".js-form-proccess";
   const privacyUrl = window.CRAZY_CART_PRIVACY_URL || `${assetBase}privacy.html`;
-  const mainSiteUrl = "https://franchise.crazy-cart.ru/";
+  const mainSiteUrl =
+    "http://franchise.crazy-cart.ru/chat_bot/?utm_source=test-avito&utm_medium={price_model}&utm_campaign={campaign_id}&utm_term={adgroup_id}&utm_content={ad_id}&rs=avito-ads_{price_model}_{campaign_id}_{adgroup_id}_{ad_id}&roistat_param1={click_id}";
   const avatarImageUrl = `${assetBase}assets/anna-avatar.jpg?v=20260513-3`;
   const cssUrl = `${assetBase}avito-chat.css?v=20260513-4`;
 
@@ -445,7 +446,7 @@
         state.leadSent = true;
         answerPanel.innerHTML = "";
         addUserMessage(payload.contact.phone);
-        await typeBotMessage("Спасибо! Анна свяжется с вами в ближайшее рабочее время и отправит материалы по франшизе.", 760);
+        await typeBotMessage("Спасибо! Мы свяжемся с вами в ближайшее рабочее время и отправим материалы по франшизе", 760);
         renderSuccessActions();
       });
     }
